d6dabeb3d83fc1c2fce339f7c93d0550d43578da
parvezrob <parvezrobinn@gmail.com>
2021-11-06 16:12:30 +0600

    first version

---
+++ routes/api.php
@@ -0,0 +1,41 @@
+<?php
+
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+//Route::resource('products', ProductController::class);
+
+
+Route::get('/products',[ProductController::class, 'index'] );
+Route::get('/products/{id}',[ProductController::class, 'show'] );
+Route::get('products/search/{name}',[ProductController::class,'search']);
+
+
+//register and auth
+Route::post('/register', [AuthController::class,'register']);
+Route::post('/login', [AuthController::class,'login']);
+Route::post('/logout', [AuthController::class,'logout']);
+
+//private routes
+Route::group(['middleware' =>['auth:sanctum']], function () {
+   
+    Route::post('/products', [ProductController::class,'store']);
+    Route::put('/products/{id}', [ProductController::class,'update']);
+    Route::delete('/products/{id}', [ProductController::class,'destroy']);
+    
+
+
+});
